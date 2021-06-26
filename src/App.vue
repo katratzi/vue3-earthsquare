350b16b1eaa822d2a1da5ebdd0c6c4d284b6cc74
katratzi <ric_clifford@hotmail.com>
2021-06-26 20:25:59 +0100

    moved userprofile into own component

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,24 @@
 <template>
-  <div id="app">
-    @{{ user.username }} - {{ fullname }}
-    <strong>Followers: {{ followers }}</strong>
-    <button v-on:click="followUser">Follow</button>
+  <div id="#app">
+    <UserProfile />
   </div>
 </template>
 <script>
-// import HelloWorld from "./components/HelloWorld.vue";
+import UserProfile from "./components/UserProfile.vue";
 
 export default {
   name: "App",
-  data() {
-    return {
-      followers: 0,
-      user: {
-        id: 1,
-        username: "rygel-xvi",
-        firstname: "dominar",
-        lastname: "rygel",
-        email: "dominar@rygel.com",
-        isAdmin: true,
-      },
-    };
-  },
-  computed: {
-    fullname() {
-      return `${this.user.firstname} ${this.user.lastname}`;
-    },
-  },
-  methods: {
-    followUser() {
-      this.followers++;
-    },
+  components: {
+    UserProfile,
   },
 };
 </script>
+
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  display: flex;
-  margin-top: 60px;
-  flex-direction: column;
-  text-align: center;
 }
 </style>
---
+++ src/components/UserProfile.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="user">
+    <h1>@{{ user.username }}</h1>
+    <h3>admin</h3>
+    <strong>Followers: {{ followers }}</strong>
+  </div>
+</template>
+<script>
+// import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "UserProfile",
+  data() {
+    return {
+      followers: 0,
+      user: {
+        id: 1,
+        username: "rygel-xvi",
+        firstname: "dominar",
+        lastname: "rygel",
+        email: "dominar@rygel.com",
+        isAdmin: true,
+      },
+    };
+  },
+  watch: {
+    // when the data changes, you get what it is now and what is was
+    followers(newFollowerCount, oldFollowerCount) {
+      if (oldFollowerCount < newFollowerCount) {
+        console.log(`${this.user.username} has gained a follower`);
+      } else {
+        console.log(`${this.user.username} has lost a follower`);
+      }
+      // else say you lost one
+    },
+  },
+  computed: {
+    fullname() {
+      return `${this.user.firstname} ${this.user.lastname}`;
+    },
+  },
+  methods: {
+    followUser() {
+      this.followers++;
+    },
+  },
+  mounted() {
+    // when this component is mounted/started, run this
+    this.followUser();
+  },
+};
+</script>
+
+<style>
+.user {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  background: 1px solid #333;
+  margin: 0em 2em 1em 2em;
+  padding: 1em;
+  border: 1px solid #333;
+  width: 200px;
+}
+.user h1 {
+  margin: 0;
+}
+</style>
