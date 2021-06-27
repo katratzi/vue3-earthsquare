14cc21076dc73910fd82f93c71f03a3efd0c1ae0
katratzi <ric_clifford@hotmail.com>
2021-06-27 18:27:01 +0100

    using the name for the route

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="#app">
-    <router-link to="/"><h2>Home</h2> </router-link>
-    <router-link to="/about"><h2>About</h2> </router-link>
+    <!-- <router-link to="/"><h2>Home</h2> </router-link>
+    <router-link to="/about"><h2>About</h2> </router-link> -->
+    <router-link :to="{ name: 'Home' }"><h2>Home</h2> </router-link>
+    <router-link :to="{ name: 'About' }"><h2>About</h2> </router-link>
     <router-view></router-view>
   </div>
 </template>
